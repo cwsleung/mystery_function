ea79e4020a2a39c1df742b8a15948bbd55fe573b
Cecilia W.S. Leung <cwsleung@lpl.arizona.edu>
2017-04-25 17:24:43 -0700

    add function

---
+++ mysteryfunction.py
@@ -0,0 +1,21 @@
+def do_something(x, y):
+    """
+    A short sentence describing what this function does.
+    
+    More description
+    
+    Parameters
+    ----------
+    x : int
+        x is a number.
+    y : int
+        y is a number.
+        
+    Returns
+    -------
+    type of return value (e.g. int, float, string, etc.)
+    int
+        The function returns the sum of x & y
+    """ 
+    z = x + y
+    print(z)
